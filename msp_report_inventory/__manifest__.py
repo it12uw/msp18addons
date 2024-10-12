5b8b9104bfb7f3160b6c879cd99e75e2f5ec22b3
IT UW <ifshuw12@gmail.com>
2024-10-12 10:41:31 +0700

    [First Commit]

---
+++ msp_report_inventory/__manifest__.py
@@ -0,0 +1,31 @@
+{
+	"name": "msp_report_inventory",
+	"version": "18.0", 
+	"depends": [
+		"base",
+		"product",
+		"stock",
+	],
+	"author": "asrent345@gmail.com", 
+	"category": "Education", 
+	'website': 'http://www.vitraining.com',
+	"description": """\
+
+Manage
+======================================================================
+
+* this is my academic information system module
+* created menu:
+* created object
+* created views
+* logic:
+
+""",
+	"data": [
+		"report/msp_surat_jalan_report.xml",
+		"report/menu.xml",
+	],
+	"installable": True,
+	"auto_install": False,
+    "application": True,
+}
